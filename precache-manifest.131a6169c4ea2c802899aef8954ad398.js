b0cd3be2224b58f526b2d2391bbeaaa8b4273a4d
Luigi R <thegaminggodlou@gmail.com>
2018-11-18 23:52:14 -0800

    Add files via upload

---
+++ precache-manifest.131a6169c4ea2c802899aef8954ad398.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/myreads-live/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/myreads-live/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/myreads-live/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "631960ce1af5bc32e9dd",
+    "url": "/myreads-live/static/js/runtime~main.631960ce.js"
+  },
+  {
+    "revision": "17e53fabf7289cc3788f",
+    "url": "/myreads-live/static/js/main.17e53fab.chunk.js"
+  },
+  {
+    "revision": "db91c86be0f9d6669fb6",
+    "url": "/myreads-live/static/js/1.db91c86b.chunk.js"
+  },
+  {
+    "revision": "17e53fabf7289cc3788f",
+    "url": "/myreads-live/static/css/main.8c9acc15.chunk.css"
+  },
+  {
+    "revision": "b0d8d8663d54536a0a16e8d8044b0140",
+    "url": "/myreads-live/index.html"
+  }
+];
